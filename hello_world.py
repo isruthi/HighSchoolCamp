a085d2e6b83c949a71659316850efed3676ed589
Sruthi Ilangovan <i.sruthi13@gmail.com>
2019-06-12 10:54:35 -0500

    Adding Tuesday's material

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+"""
+title: hello_world
+author: isruthi
+date: 2019-06-11 10:52
+"""
+print("Hello world")
---
+++ random_practice.py
@@ -0,0 +1,19 @@
+"""
+title: random_practice
+author: isruthi
+date: 2019-06-12 09:53
+"""
+
+import random
+
+name = input("Enter your name:")
+salary = input("Enter your salary:")
+salary = int(salary)
+print(f"{name}, your current salary is ${salary}.")
+
+raise_per = (random.randint(0, 100))
+print(f"Your raise is {raise_per}% of ${salary}")
+
+raise_amount = int((salary * raise_per * 0.01)+salary)
+
+print(f"{name}, your new salary is ${raise_amount}.")
